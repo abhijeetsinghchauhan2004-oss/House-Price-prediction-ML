42f65f0c8d67a18645707eaa8b9c235e0d1b27f8
abhijeetsinghchauhan2004-oss <abhijeetsinghchauhan2004@gmail.com>
2025-12-28 15:02:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ import time
 from sklearn.datasets import fetch_california_housing
 
 st.title('🏠 House Price prediction using ML')
-st.image('https://img.freepik.com/free-vector/charming-house-with-tree-illustration_1308-176337.jpg?semt=ais_hybrid&w=740&q=80')
+st.image('https://i.pinimg.com/originals/9e/72/41/9e72419aa97348563f8f917f6b2ffe7b.gif')
+
